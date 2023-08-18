c61185daa14bf7b89efd7d14a15718909c6dabf0
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-08-18 11:35:54 +0200

    Add link to homepage

--- src/pages/AnimalPage.tsx
+++ src/pages/AnimalPage.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Animal } from '../models/Animals';
-
 import { useParams } from 'react-router-dom';
 import AnimalDetail from '../components/AnimalDetail';
+import { Link } from 'react-router-dom';
 
 const AnimalPage: React.FC = () => {
     const [animal, setAnimal] = useState<Animal | null>(null);
@@ -16,9 +16,11 @@ const AnimalPage: React.FC = () => {
 
     return (
         <div className="animal-page-container">   
-            {animal && <AnimalDetail animal={animal} />}
+            {animal && <AnimalDetail animal={animal} />}          
+            <Link to="/">Back to homepage</Link>
         </div>
     );
+    
 };
 
 export default AnimalPage;
