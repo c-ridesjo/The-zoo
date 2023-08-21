e8c6218a05e35a3b9fedc54395f5629c3caa045c
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-08-21 10:18:00 +0200

    Error handling for images

--- src/components/AnimalBox.tsx
+++ src/components/AnimalBox.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Animal } from '../models/Animals';
 import { Link } from 'react-router-dom';
+import fallbackImage from '../assets/404.jpg';
 
 interface AnimalBoxProps {
     animal: Animal;
 }
 
 const AnimalBox: React.FC<AnimalBoxProps> = ({ animal }) => {
+    const [imageFailed, setImageFailed] = useState(false);
+
+    const handleImageError = () => {
+        setImageFailed(true);
+    };
+
+    const imgSrc = imageFailed ? fallbackImage : animal.imageUrl;
+
     return (
         <Link to={`/animal/${animal.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="animal-box">
                 <h2>{animal.name}</h2>
-                <img className="img" src={animal.imageUrl} alt={animal.name} />
+                <img className="img" src={imgSrc} alt={animal.name} onError={handleImageError}/>
                 <p>{animal.shortDescription}</p>
             </div>
         </Link>
